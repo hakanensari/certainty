97032ee10d38900038a33034e7a46bd9c51b9695
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-08-19 17:32:30 +0100

    add #to_bool to core classes

--- lib/boolean.rb
+++ lib/boolean.rb
@@ -1,29 +1,67 @@
-require 'boolean/version'
-
 # = Boolean
 #
-# An object that represents truth.
+# Boolean is an object that represents truth.
 module Boolean; end
 
 [TrueClass, FalseClass].each { |klass| klass.send :include, Boolean }
 
+# == Conversion methods.
+#
+# The truth of an object, by default, is not knowable.
+class Object
+  def to_bool
+    nil
+  end
+  alias to_b to_bool
+end
+
+# The truth of a boolean object is self-referential.
+module Boolean
+  def to_bool
+    self
+  end
+  alias to_b to_bool
+end
+
+# We evaluate the truth of a string based on the following convention:
+#
+# If it is _1_ or a lower- or upper-cased _true_, it evaluates to
+# +true+. If it is a _0_ or a lower- or upper-cased _false_, it
+# evaluates to +false+.
+#
+# Otherwise its truth is not knowable.
+class String
+  def to_bool
+    {
+      '1'     => true,
+      'true'  => true,
+      '0'     => false,
+      'false' => false
+    }[downcase]
+  end
+  alias to_b to_bool
+end
+
+# We evaluate the truth of a number based on the following convention:
+#
+# If it is equal to _1_, it evaluates to +true+. If it is equal to _0_,
+# it evaluates to +false+.
+#
+# Otherwise its truth is not knowable.
+class Numeric
+  def to_bool
+    {
+      1.0 => true,
+      0.0 => false
+    }[to_f]
+  end
+  alias to_b to_bool
+end
+
 module Kernel
-  # Converts +arg+ to a +Boolean+ or +nil+ if its truth is not
+  # Converts _arg_ to a +Boolean+ or +nil+ if its truth is not
   # knowable.
   def Boolean(arg)
-    {
-      true    => true,
-      'true'  => true,
-      'TRUE'  => true,
-      '1'     => true,
-      1       => true,
-      1.0     => true,
-      false   => false,
-      'false' => false,
-      'FALSE' => false,
-      '0'     => false,
-      0       => false,
-      0.0     => false
-    }[arg]
+    arg.to_bool rescue nil
   end
 end
--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -12,6 +12,23 @@ class TestBoolean < Test::Unit::TestCase
     assert_kind_of Boolean, false
   end
 
+  def test_boolean_representation
+    assert_equal true,  true.to_bool
+    assert_equal false, false.to_b
+
+    assert_equal true,  'true'.to_b
+    assert_equal true,  '1'.to_b
+    assert_equal false, 'FALSE'.to_b
+    assert_equal false, '0'.to_b
+    assert_nil   'foo'.to_b
+
+    assert_equal true,  1.to_b
+    assert_equal false, 0.0.to_b
+    assert_nil   2.to_b
+
+    assert_nil   nil.to_b
+  end
+
   def test_conversion
     assert_equal true,  Boolean(true)
     assert_equal true,  Boolean('1')
